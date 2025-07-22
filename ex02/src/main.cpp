cfbc365326d88059d100ffaa56b0f8f6e8a7826c
samlard <sami.soumillion@gmail.com>
2025-07-22 13:55:06 +0200

    added test on my ex02/main.cpp

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -2,6 +2,10 @@
 
 int main(void)
 {
+    // int *i;
+    // int a = 42;
+    // int &b;
+    // b = a;
     std::string str = "HI THIS BRAIN";
     std::string *ptr = &str;
     std::string &ref = str;
@@ -12,4 +16,17 @@ int main(void)
     std::cout << "str content: " << str << std::endl;
     std::cout << "ptr content: " << *ptr << std::endl;
     std::cout << "ref content: " << ref << std::endl;
+
+    *ptr = "HELLOW MISTER BRAIN";
+    std::cout << "str content after change: " << str << std::endl;
+    std::cout << "ptr content after change: " << *ptr << std::endl;
+    std::cout << "ref content after change: " << ref << std::endl;
+
+    ref = "coucou";
+    std::cout << "str content after ref change: " << str << std::endl
+              << "ptr content after ref change: " << *ptr << std::endl
+              << "ref content after ref change: " << ref << std::endl;
+
+    // i = &a;
+    // std::cout << "value of i: " << *i << std::endl;
 }
